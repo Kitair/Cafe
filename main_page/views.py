0d09183d0e5c884928a97833228b0d613f7ff945
Kitair <somegrey9@gmail.com>
2021-07-09 03:24:33 +0300

    Ivan: Completion menu development

--- main_page/views.py
+++ main_page/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Category, Dish
 
 # Create your views here.
+
+
+def main_view(request):
+    categories = Category.objects.filter(is_visible=True).order_by('-category_order')
+    dish = Dish.objects.filter(is_visible=True).order_by('-dish_order')
+    return render(request, 'index.html', context={'categories': categories, 'dish': dish})
+
